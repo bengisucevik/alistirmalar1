58c772b327733caad0207ba5238018aef7b85622
bengisucevik <72698938+bengisucevik@users.noreply.github.com>
2020-11-21 21:44:14 +0300

    Add files via upload

---
+++ soru1.12.py
@@ -0,0 +1,4 @@
+for i in range(1500,2020):#bu yıllar arasında doğması muhtemel
+    if int(str(i)[0])+int(str(i)[1])+int(str(i)[2])+int(str(i)[3]) == 2005-i:
+                                                            print(i)
+
